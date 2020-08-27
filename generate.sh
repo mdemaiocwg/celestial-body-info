2188ab63e410e9a960a7ed4d55caaa7d11de8507
Matthew DeMaio <MDemaio@NY.CORP.INT>
2020-08-27 09:24:29 -0400

    Updated generate script

--- generate.sh
+++ generate.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-protoc greet/greetpb/greet.proto --go_out=plugins=grpc:.
-protoc calculator/calculatorpb/calculator.proto --go_out=plugins=grpc:.
-protoc blog/blogpb/blog.proto --go_out=plugins=grpc:.
+protoc planet/planetpb/planet.proto --go_out=plugins=grpc:.
